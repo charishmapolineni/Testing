fea643ffb2177e9198fbb1024bd3b2267e38cdbd
Rakesh Bolleni <rakeshbolleni@Rakeshs-MacBook-Pro.local>
2019-02-28 22:41:49 -0500

    basics of java

---
+++ Demo.java
@@ -0,0 +1,13 @@
+public class Demo {
+public static void main(String args[])
+{
+int a=10;
+int b=20;
+int c;
+c=a+b;
+String name ="rakesh";
+System.out.println("print addition of two numbers" + c);
+
+}
+
+}
